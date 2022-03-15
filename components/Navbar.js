90ece1946bdd6c426e3e23e00b54129ffb000e49
fluturecode <fluturecode@gmail.com>
2022-03-15 13:06:28 -0400

    added react-icons package

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,24 +1,50 @@
-import Image from "next/image";
+import Image from 'next/image';
+import { BsPerson } from 'react-icons/bs';
 import avatar from '../assets/avatar.png';
 
+const currentAccount = process.env.NEXT_WALLET_ADD;
+
 const style = {
-  wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20 `
-}
+  wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20 `,
+  leftMenu: `flex gap-3`,
+  logo: `text-3xl text-white flex cursor-pointer mr-16`,
+  menuItem: `text-lg text-white font-medium flex items-center mx-4 cursor-pointer`,
+  rightMenu: `flex gap-3 items-center`,
+  userImageContainer: `mr-2`,
+  userImage: `h-10 w-10 mr-4 rounded-full p-px object-cover cursor-pointer`,
+  loginButton: `flex items-center cursor-pointer rounded-full hover:bg-[#333333] px-4 py-1`,
+  loginText: `ml-2`,
+};
 
 export const Navbar = () => {
-  return <div  className={style.wrapper}>
-    <div className={style.leftMenu}>
-      <div className={style.logo}>Uber</div>
-      <div className={style.menuItem}>Ride</div>
-      <div className={style.menuItem}>Drive</div>
-      <div className={style.menuItem}>More</div>
-    </div>
-    <div className={style.rightMenu}>
-      <div className={style.menuItem}>Help</div>
-      <div className={style.menuItem}>Liz</div>
-      <div className={style.userImageContainer}>
-        <Image src={avatar} width={40} height={40}/>
+  return (
+    <div className={style.wrapper}>
+      <div className={style.leftMenu}>
+        <div className={style.logo}>Uber</div>
+        <div className={style.menuItem}>Ride</div>
+        <div className={style.menuItem}>Drive</div>
+        <div className={style.menuItem}>More</div>
+      </div>
+      <div className={style.rightMenu}>
+        <div className={style.menuItem}>Help</div>
+        <div className={style.menuItem}>Liz</div>
+        <div className={style.userImageContainer}>
+          <Image
+            className={style.userImage}
+            src={avatar}
+            width={40}
+            height={40}
+          />
+        </div>
+        {currentAccount ? (
+          <div>{currentAccount}</div>
+        ) : (
+          <div className={style.loginButton}>
+            <BsPerson />
+            <span className={style.loginText}>Log in</span>
+          </div>
+        )}
       </div>
     </div>
-  </div>
+  );
 };
