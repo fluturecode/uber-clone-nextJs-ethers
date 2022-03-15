d41b3763bbe70e0d8fd14a4405026c9b383c5c57
fluturecode <fluturecode@gmail.com>
2022-03-15 12:06:02 -0400

    added avatar to Navbar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import avatar from '../assets/avatar.png';
+
 const style = {
   wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20 `
 }
@@ -10,5 +13,12 @@ export const Navbar = () => {
       <div className={style.menuItem}>Drive</div>
       <div className={style.menuItem}>More</div>
     </div>
-  </div>;
+    <div className={style.rightMenu}>
+      <div className={style.menuItem}>Help</div>
+      <div className={style.menuItem}>Liz</div>
+      <div className={style.userImageContainer}>
+        <Image src={avatar} width={40} height={40}/>
+      </div>
+    </div>
+  </div>
 };
