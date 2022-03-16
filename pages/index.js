13c04a942f849aef043534216f2cc0e7e7e3d80d
fluturecode <fluturecode@gmail.com>
2022-03-15 21:20:47 -0400

    added mapbox-gl

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
-import { Map, Navbar } from "../components"
+import { Map, Navbar } from '../components';
 
 const style = {
-  wrapper: `h-screen w-screen flex flex-col`
-}
+  wrapper: `h-screen w-screen flex flex-col`,
+};
 
 export default function Home() {
   return (
     <div className={style.wrapper}>
-      <Navbar/>
+      <Navbar />
       <div className={style.main}>
-      <Map/>
+        <Map />
       </div>
       <div className={style.rideRequestContainer}>
         <div className={style.rideRequest}>
-          /* location selector */
-          /* confirm ride */
+          /* location selector */ /* confirm ride */
         </div>
       </div>
     </div>
-  )
+  );
 }
